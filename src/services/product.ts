ec1eacc3c9b94f8d675f5375f8c64869c91c9222
cafora <cafora32@gmail.com>
2025-04-12 19:11:54 +0530

    Update product.ts

--- src/services/product.ts
+++ src/services/product.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Service = {
     createProduct: (formData: FormData, adminAuthToken: string) => {
-        return axios.post(`http://localhost:4000/admin/create-product`, formData, {
+        return axios.post(`https://server-blpu.onrender.com/admin/create-product`, formData, {
             headers: {
                 'Authorization': `Bearer ${adminAuthToken}`,
                 'Content-Type': 'multipart/form-data',
@@ -10,7 +10,7 @@ const Service = {
         })
     },
     updateProduct: (productToEdit: any, formData: FormData, adminAuthToken: string) => {
-        return axios.put(`http://localhost:4000/admin/update-product/${productToEdit._id}`, formData, {
+        return axios.put(`https://server-blpu.onrender.com/admin/update-product/${productToEdit._id}`, formData, {
             headers: {
                 'Authorization': `Bearer ${adminAuthToken}`,
                 'Content-Type': 'multipart/form-data',
